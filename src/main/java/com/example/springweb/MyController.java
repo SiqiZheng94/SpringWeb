5c361511fd44a3d096b9d617914f22e1c35a2bd8
siqizheng <cn.siqizheng@gmail.com>
2023-11-22 11:59:59 +0100

    post get delete

--- src/main/java/com/example/springweb/MyController.java
+++ src/main/java/com/example/springweb/MyController.java
@@ -1,8 +1,10 @@
 package com.example.springweb;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 @RestController
 public class MyController {
@@ -15,5 +17,28 @@ public class MyController {
     public String sayHello2(@PathVariable String name){
         return "Hello, " + name + "!";
     }
+    private List<Message> messages = new ArrayList<>();
 
+    @GetMapping("/api/messages")
+    public List<Message> getAllMessages() {
+        return messages;
+    }
+
+    @PostMapping("/api/messages")
+    public void addMessage(@RequestBody Message message) {
+        messages.add(message);
+    }
+
+    @DeleteMapping("/api/messages/{id}")
+    public void deleteMessage(@PathVariable String id) {
+        // Iterate through the list and remove the message with the specified ID
+
+        Iterator<Message> iterator = messages.iterator();
+        while (iterator.hasNext()) {
+            Message msg = iterator.next();
+            if (msg.getId().equals(id)) {
+                iterator.remove();
+            }
+        }
+    }
 }
