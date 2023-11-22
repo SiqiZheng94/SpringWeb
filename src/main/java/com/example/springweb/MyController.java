11fddfb89b74c8763f9d018947a6e9e96e1ae7f7
siqizheng <cn.siqizheng@gmail.com>
2023-11-22 11:07:35 +0100

    hello controller

--- src/main/java/com/example/springweb/MyController.java
+++ src/main/java/com/example/springweb/MyController.java
@@ -1,13 +1,19 @@
 package com.example.springweb;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
     @GetMapping("/api/hello")
     public String sayHello(){
-        return "hello, world!";
+        return "Hello, world!";
+    }
+
+    @GetMapping("/api/hello/{name}")
+    public String sayHello2(@PathVariable String name){
+        return "Hello, " + name + "!";
     }
 
 }
